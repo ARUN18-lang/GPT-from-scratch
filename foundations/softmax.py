4c8bfb3f316ed5980680b3c5343eaf4f2fb711cb
ARUNKUMAR <arun559703@gmail.com>
2026-08-21 20:44:53 +0530

    Update Softmax function (Math Foundations 3/6)

--- foundations/softmax.py
+++ foundations/softmax.py
@@ -3,16 +3,23 @@ from numpy.typing import NDArray
 
 
 class Solution:
-    # sigmoid and softmax produces output between [0-1], then why not sigmoid instead of softmax.
-    # Sigmoid handles independent binary choices. Softmax handles mutually exclusive multi-class choices by forcing all output scores to add up to exactly 1
-    # example -> sigmoid produces 0 or 1 binary choices, softmax produces list of probs, the one with higher probs wins 
-    # probs = [0.2, 0.6, 0.1, 0.1] - max(probs) = 0.6
+
+    def temperature(self, probs: NDArray[np.float64], T: float) -> NDArray[np.float64]:
+        maxi = max(probs)
+        denominator = 0
+        for j in probs:
+            denominator += 2.71828**((j-maxi)/T)
+        
+        for ind, logit in enumerate(probs):
+            probs[ind] = np.round(2.71828**((logit - maxi)/T) / denominator, 4)
+        
+        return probs
+
     def softmax(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
         # z is a 1D NumPy array of logits
         # Hint: subtract max(z) for numerical stability before computing exp
         # return np.round(your_answer, 4)
         maxi = max(z)
-        print(maxi)
         denominator = 0
         for j in z:
             denominator += 2.71828**(j-maxi)
@@ -20,7 +27,10 @@ class Solution:
         for ind, logit in enumerate(z):
             z[ind] = np.round(2.71828**(logit - maxi) / denominator, 4)
         
+        print("Temperature sampling: ", self.temperature(z.copy(), 0.5))
         return z
-            
+    
+
 
         
+
